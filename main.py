b73bb145e6585ddbc0cc9b73cbe704b9c4b2b43c
hick-hpe <hpalermoemerick@gmail.com>
2025-08-14 14:10:38 -0300

    feat: workflow 'hello'

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-def hello_world():
-    print('Hello World')
-
 def func(x):
     return x + 1
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,9 +1,10 @@
-from main import func, hello_world
+from main import func
+from hello import hello_world
 
 def test_hello_world(capsys):
     hello_world()
     captured = capsys.readouterr()
     assert captured.out == "Hello World\n"
-    
+
 def test_answer():
     assert func(3) == 4
