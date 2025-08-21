1d583e3377831ab40b14a17db4797ef2b15fc262
hick-hpe <hpalermoemerick@gmail.com>
2025-08-21 15:28:45 -0300

    feat: branch atividade

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+"""
+Exercício
+
+Implementar uma calculadora com as quatro operações básicas (adição, subtraç, multiplicação e divisão) e escrever, para cada operação, 3 casos de testes.
+Na divisão, incluir a divisão por zero.
+"""
+
+
